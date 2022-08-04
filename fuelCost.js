eddf6ccc81d7bcd300df72673bf09d5332fd9255
dev-alt <53392022+dev-alt@users.noreply.github.com>
2022-08-04 12:58:35 +1200

    Add files via upload

---
+++ fuelCost.js
@@ -0,0 +1,9 @@
+function calculate() {
+  let carEconomy = document.getElementById("carEconomy").value;
+  let distance = document.getElementById("distance").value;
+  let fuelCost = document.getElementById("fuelCost").value;
+
+  let litreCost = (carEconomy * fuelCost) / 100;
+  document.getElementById("display").innerHTML =
+    "This trip cost you $" + Math.round(litreCost * distance);
+}
